99a6279e539b0ba0919e0940b0a486ead4c0c712
Victor Bofill <terranvas@gmail.com>
2018-02-05 14:16:37 -0800

    Object template created, calculating cookies per hour created, working on function to create a list.

--- app.js
+++ app.js
@@ -11,3 +11,30 @@
 
 // design an entire front-end website
 
+let storePDX = {
+    minCust: 23,
+    maxCust: 65,
+    avgCookiesPerCust: 6.3,
+    custHour: function() {
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+     cookiesPurchasedPerHour: []
+};
+
+const calculateCookiesPerHour = function(object) {
+    for (let i = 0; i < 15; i++) {
+        object.cookiesPurchasedPerHour[i] = object.custHour();
+    }
+}
+
+const populateList = function(object) {
+    const cookieSection = document.getElementById('cookieSection');
+    const newList = [];
+    for (let i = 0; i < 15; i++) {
+        newList.push(object.cookiesPurchasedPerHour[i]);
+    }
+    
+    cookieSection.appendChild(newList);
+}
