3d6878ef198b43201b3912c34034c51cfd4f9118
Victor Bofill <terranvas@gmail.com>
2018-02-05 13:26:34 -0800

    Initial project files added.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Salmon Cookie
+## Overview
+This is the repo for the Calmon Cookie website, both front- and back-end.
+
+## Getting Started
+Devs: Fork the repo and get coding!
+
+Customers: Please visit us on the web at salmoncookies.com.
---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// hours 6a-8p
+// min customers per hour
+// max customers per hour
+// avg cookies per customer
+
+// be able to add and remove locations
+// easily modify internal data
+// data presented nicely formatted
+
+// design an entire front-end website
+
