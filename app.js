21b9c7362da0850d17e05f30e799504fc98ca59f
Victor Bofill <terranvas@gmail.com>
2018-02-05 14:49:23 -0800

    List population complete.

--- app.js
+++ app.js
@@ -11,7 +11,10 @@
 
 // design an entire front-end website
 
+const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm']
+
 let storePDX = {
+    storeName: 'PDX Airport',
     minCust: 23,
     maxCust: 65,
     avgCookiesPerCust: 6.3,
@@ -24,17 +27,33 @@ let storePDX = {
 };
 
 const calculateCookiesPerHour = function(object) {
+    let totalCookies = 0;
     for (let i = 0; i < 15; i++) {
         object.cookiesPurchasedPerHour[i] = object.custHour();
+        totalCookies += object.cookiesPurchasedPerHour[i];
     }
+
+    object.cookiesPurchasedPerHour.push(totalCookies);
 }
 
 const populateList = function(object) {
-    const cookieSection = document.getElementById('cookieSection');
-    const newList = [];
+    const cookieSection = document.getElementById('cookiesection');
+
+    const newStoreName = document.createElement('p');
+    newStoreName.textContent = object.storeName;
+    cookieSection.appendChild(newStoreName);
+
+    const newUl = document.createElement('ul');
+    cookieSection.appendChild(newUl);
+
+    const liArray = [];
     for (let i = 0; i < 15; i++) {
-        newList.push(object.cookiesPurchasedPerHour[i]);
+        liArray.push(object.cookiesPurchasedPerHour[i]);
+        const newLi = document.createElement('li');
+        newLi.textContent = hours[i] + ': ' + object.cookiesPurchasedPerHour[i] + ' cookies';
+        newUl.appendChild(newLi);
     }
-    
-    cookieSection.appendChild(newList);
 }
+
+calculateCookiesPerHour(storePDX);
+populateList(storePDX);
