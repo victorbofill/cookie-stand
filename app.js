671caec8783ce40651c63af6b40a58e5bdbc2d1c
Victor Bofill <terranvas@gmail.com>
2018-02-06 13:39:42 -0800

    JS complete.

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 'use strict';
 
-const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total'];
+const hours = [
+    '', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm',
+    '5:00pm', '6:00pm', '7:00pm', '8:00pm', 'Daily Location Total'
+];
+
+const hourTotal = ['Totals By Hour',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+const createTable = function() {
+    const cookieSection = document.getElementById('cookie-section');
+    const table = document.createElement('table');
+    cookieSection.appendChild(table);
+    table.setAttribute('id', 'cookie-table');
+};
+
+const buildTableHeader = function() {
+    const table = document.getElementById('cookie-table');
+    const tableHead = document.createElement('thead');
+    table.appendChild(tableHead);
+
+    const tableRow = document.createElement('tr');
+    tableHead.appendChild(tableRow);
+
+    for (let i = 0; i < hours.length; i++) {
+        const tableCell = document.createElement('th');
+        tableHead.appendChild(tableCell);
+        tableCell.textContent = hours[i];
+    }
+};
+
+const buildTableFooter = function () {
+    const table = document.getElementById('cookie-table');
+    const tableFoot = document.createElement('tfoot');
+    table.appendChild(tableFoot);
+
+    const tableRow = document.createElement('tr');
+    tableFoot.appendChild(tableRow);
+
+    for (let i = 0; i < hourTotal.length; i++) {
+        const tableCell = document.createElement('td');
+        tableFoot.appendChild(tableCell);
+        tableCell.textContent = hourTotal[i];
+    }
+};
 
 function Store(storeName, minCust, maxCust, avgCookiesPerCust) {
     this.storeName = storeName;
@@ -21,14 +63,16 @@ Store.prototype.populateCookiesArray = function() {
     let totalCookies = 0;
 
     for (let i = 0; i < 15; i++) {
-        this.estCookiesPerHour[i] = this.calcCookiesHour();
+        const cookiesPerHour = this.calcCookiesHour();
+        this.estCookiesPerHour[i] = cookiesPerHour;
         totalCookies += this.estCookiesPerHour[i];
+        hourTotal[(i + 1)] += cookiesPerHour;
     }
 
     this.estCookiesPerHour.push(totalCookies);
 };
 
-Store.prototype.createCookieTable = function () {
+Store.prototype.render = function () {
     const cookieSection = document.getElementById('cookie-table');
     const newTableRow = document.createElement('tr');
     cookieSection.appendChild(newTableRow);
@@ -46,7 +90,7 @@ Store.prototype.createCookieTable = function () {
 
 const activateStore = function(object) {
     object.populateCookiesArray();
-    object.createCookieTable();
+    object.render();
 };
 
 const storePDX = new Store('PDX Airport', 23, 65, 6.3);
@@ -55,8 +99,15 @@ const storePowells = new Store('Powell\'s', 11, 38, 3.7);
 const storeStJohns = new Store('St. John\'s', 20, 38, 2.3);
 const storeWaterfront = new Store('Waterfront', 2, 16, 4.6);
 
-activateStore(storePDX);
-activateStore(storePioneer);
-activateStore(storePowells);
-activateStore(storeStJohns);
-activateStore(storeWaterfront);
+const buildTable = function() {
+    createTable();
+    buildTableHeader();
+    activateStore(storePDX);
+    activateStore(storePioneer);
+    activateStore(storePowells);
+    activateStore(storeStJohns);
+    activateStore(storeWaterfront);
+    buildTableFooter();    
+};
+
+buildTable();
