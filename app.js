1e4bc19c39784f1ca68d070b0cb140d8ba0a2c88
Victor Bofill <terranvas@gmail.com>
2018-02-06 11:49:00 -0800

    Initial work with constructor function. Removed literal objects and created methods.

--- app.js
+++ app.js
@@ -2,90 +2,37 @@
 
 const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total'];
 
-const storePDX = {
-    storeName: 'PDX Airport',
-    minCust: 23,
-    maxCust: 65,
-    avgCookiesPerCust: 6.3,
-    estCookiesPerHour: [],
-    calcCookiesHour: function () {
-        const min = Math.ceil(this.minCust);
-        const max = Math.floor(this.maxCust);
-        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return Math.floor(randomNumber * this.avgCookiesPerCust);
-    }
+function Store(storeName, minCust, maxCust, avgCookiesPerCust) {
+    this.storeName = storeName;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.avgCookiesPerCust = avgCookiesPerCust;
+    this.estCookiesPerHour = [];
 };
 
-const storePioneer = {
-    storeName: 'Pioneer Square',
-    minCust: 3,
-    maxCust: 24,
-    avgCookiesPerCust: 1.2,
-    estCookiesPerHour: [],
-    calcCookiesHour: function () {
-        const min = Math.ceil(this.minCust);
-        const max = Math.floor(this.maxCust);
-        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return Math.floor(randomNumber * this.avgCookiesPerCust);
-    }
+Store.prototype.calcCookiesHour = function() {
+    const min = Math.ceil(this.minCust);
+    const max = Math.floor(this.maxCust);
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(randomNumber * this.avgCookiesPerCust);
 };
 
-const storePowells = {
-    storeName: 'Powell\'s',
-    minCust: 11,
-    maxCust: 38,
-    avgCookiesPerCust: 3.7,
-    estCookiesPerHour: [],
-    calcCookiesHour: function () {
-        const min = Math.ceil(this.minCust);
-        const max = Math.floor(this.maxCust);
-        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return Math.floor(randomNumber * this.avgCookiesPerCust);
-    }
-};
-
-const storeStJohns = {
-    storeName: 'St. John\'s',
-    minCust: 20,
-    maxCust: 38,
-    avgCookiesPerCust: 2.3,
-    estCookiesPerHour: [],
-    calcCookiesHour: function () {
-        const min = Math.ceil(this.minCust);
-        const max = Math.floor(this.maxCust);
-        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return Math.floor(randomNumber * this.avgCookiesPerCust);
-    }
-};
-
-const storeWaterfront = {
-    storeName: 'Waterfront',
-    minCust: 2,
-    maxCust: 16,
-    avgCookiesPerCust: 4.6,
-    estCookiesPerHour: [],
-    calcCookiesHour: function () {
-        const min = Math.ceil(this.minCust);
-        const max = Math.floor(this.maxCust);
-        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return Math.floor(randomNumber * this.avgCookiesPerCust);
-    }
-};
-
-const createCookiesList = function(object) {
+Store.prototype.populateCookiesArray = function() {
     let totalCookies = 0;
 
     for (let i = 0; i < 15; i++) {
-        object.estCookiesPerHour[i] = object.calcCookiesHour();
-        totalCookies += object.estCookiesPerHour[i];
+        this.estCookiesPerHour[i] = this.calcCookiesHour();
+        totalCookies += this.estCookiesPerHour[i];
     }
 
-    object.estCookiesPerHour.push(totalCookies);
+    this.estCookiesPerHour.push(totalCookies);
+};
 
+Store.prototype.createCookiesList = function() {
     const cookieSection = document.getElementById('cookiesection');
 
     const newStoreName = document.createElement('p');
-    newStoreName.textContent = object.storeName;
+    newStoreName.textContent = this.storeName;
     cookieSection.appendChild(newStoreName);
 
     const newUl = document.createElement('ul');
@@ -93,15 +40,19 @@ const createCookiesList = function(object) {
 
     const liArray = [];
     for (let i = 0; i < 16; i++) {
-        liArray.push(object.estCookiesPerHour[i]);
+        liArray.push(this.estCookiesPerHour[i]);
         const newLi = document.createElement('li');
-        newLi.textContent = hours[i] + ': ' + object.estCookiesPerHour[i] + ' cookies';
+        newLi.textContent = hours[i] + ': ' + this.estCookiesPerHour[i] + ' cookies';
         newUl.appendChild(newLi);
     }
 };
 
-createCookiesList(storePDX);
-createCookiesList(storePioneer);
-createCookiesList(storePowells);
-createCookiesList(storeStJohns);
-createCookiesList(storeWaterfront);
+const storePDX = new Store('PDX Airport', 23, 65, 6.3);
+
+const storePioneer = new Store('Pioneer Square', 3, 24, 1.2);
+
+const storePowells = new Store('Powell\'s', 11, 38, 3.7);
+
+const storeStJohns = new Store('St. John\'s', 20, 38, 2.3);
+
+const storeWaterfront = new Store('Waterfront', 2, 16, 4.6);
