e4b23ac2d3d49677df6603ee415f81fd6bb55bc8
Victor Bofill <terranvas@gmail.com>
2018-02-06 10:10:25 -0800

    Resolved all lint errors.

--- app.js
+++ app.js
@@ -1,76 +1,76 @@
 'use strict';
 
-const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total']
+const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total'];
 
-let storePDX = {
+const storePDX = {
     storeName: 'PDX Airport',
     minCust: 23,
     maxCust: 65,
     avgCookiesPerCust: 6.3,
     estCookiesPerHour: [],
     calcCookiesHour: function () {
-        let min = Math.ceil(this.minCust);
-        let max = Math.floor(this.maxCust);
-        let randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return Math.floor(randomNumber * this.avgCookiesPerCust);
     }
 };
 
-let storePioneer = {
+const storePioneer = {
     storeName: 'Pioneer Square',
     minCust: 3,
     maxCust: 24,
     avgCookiesPerCust: 1.2,
     estCookiesPerHour: [],
     calcCookiesHour: function () {
-        let min = Math.ceil(this.minCust);
-        let max = Math.floor(this.maxCust);
-        let randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return Math.floor(randomNumber * this.avgCookiesPerCust);
     }
-}
+};
 
-let storePowells = {
+const storePowells = {
     storeName: 'Powell\'s',
     minCust: 11,
     maxCust: 38,
     avgCookiesPerCust: 3.7,
     estCookiesPerHour: [],
     calcCookiesHour: function () {
-        let min = Math.ceil(this.minCust);
-        let max = Math.floor(this.maxCust);
-        let randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return Math.floor(randomNumber * this.avgCookiesPerCust);
     }
-}
+};
 
-let storeStJohns = {
+const storeStJohns = {
     storeName: 'St. John\'s',
     minCust: 20,
     maxCust: 38,
     avgCookiesPerCust: 2.3,
     estCookiesPerHour: [],
     calcCookiesHour: function () {
-        let min = Math.ceil(this.minCust);
-        let max = Math.floor(this.maxCust);
-        let randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return Math.floor(randomNumber * this.avgCookiesPerCust);
     }
-}
+};
 
-let storeWaterfront = {
+const storeWaterfront = {
     storeName: 'Waterfront',
     minCust: 2,
     maxCust: 16,
     avgCookiesPerCust: 4.6,
     estCookiesPerHour: [],
     calcCookiesHour: function () {
-        let min = Math.ceil(this.minCust);
-        let max = Math.floor(this.maxCust);
-        let randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+        const min = Math.ceil(this.minCust);
+        const max = Math.floor(this.maxCust);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return Math.floor(randomNumber * this.avgCookiesPerCust);
     }
-}
+};
 
 const createCookiesList = function(object) {
     let totalCookies = 0;
@@ -98,7 +98,7 @@ const createCookiesList = function(object) {
         newLi.textContent = hours[i] + ': ' + object.estCookiesPerHour[i] + ' cookies';
         newUl.appendChild(newLi);
     }
-}
+};
 
 createCookiesList(storePDX);
 createCookiesList(storePioneer);
